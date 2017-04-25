3fb5f772fd9bd390ae415e3add1fd25807cd8d6d
Bria Wallace <brwallace@groupon.com>
2017-04-25 15:25:41 -0700

    Added Bart stop abbreviations and station names

---
+++ BartApp/GlobalVariables.swift
@@ -0,0 +1,58 @@
+//
+//  GlobalVariables.swift
+//  BartApp
+//
+//  Created by Bria Wallace on 4/25/17.
+//  Copyright © 2017 Bria Wallace. All rights reserved.
+//
+
+import Foundation
+
+var bartStops: [[String: String]]! = [
+    ["code":"12th", "name":"12th St. Oakland City Center"],
+    ["code":"16th", "name":"16th St. Mission (SF)"],
+    ["code":"19th", "name":"19th St. Oakland"],
+    ["code":"24th", "name":"24th St. Mission (SF)"],
+    ["code":"ashb", "name":"Ashby (Berkeley)"],
+    ["code":"balb", "name":"Balboa Park (SF)"],
+    ["code":"bayf", "name":"Bay Fair (San Leandro)"],
+    ["code":"cast", "name":"Castro Valley"],
+    ["code":"civc", "name":"Civic Center (SF)"],
+    ["code":"cols", "name":"Coliseum"],
+    ["code":"colm", "name":"Colma"],
+    ["code":"conc", "name":"Concord"],
+    ["code":"daly", "name":"Daly City"],
+    ["code":"dbrk", "name":"Downtown Berkeley"],
+    ["code":"dubl", "name":"Dublin/Pleasanton"],
+    ["code":"deln", "name":"El Cerrito del Norte"],
+    ["code":"plza", "name":"El Cerrito Plaza"],
+    ["code":"embr", "name":"Embarcadero (SF)"],
+    ["code":"frmt", "name":"Fremont"],
+    ["code":"ftvl", "name":"Fruitvale (Oakland)"],
+    ["code":"glen", "name":"Glen Park (SF)"],
+    ["code":"hayw", "name":"Hayward"],
+    ["code":"lafy", "name":"Lafayette"],
+    ["code":"lake", "name":"Lake Merritt (Oakland)"],
+    ["code":"mcar", "name":"MacArthur (Oakland)"],
+    ["code":"mlbr", "name":"Millbrae"],
+    ["code":"mont", "name":"Montgomery St. (SF)"],
+    ["code":"nbrk", "name":"North Berkeley"],
+    ["code":"ncon", "name":"North Concord/Martinez"],
+    ["code":"oakl", "name":"Oakland Int'l Airport"],
+    ["code":"orin", "name":"Orinda"],
+    ["code":"pitt", "name":"Pittsburg/Bay Point"],
+    ["code":"phil", "name":"Pleasant Hill"],
+    ["code":"powl", "name":"Powell St. (SF)"],
+    ["code":"rich", "name":"Richmond"],
+    ["code":"rock", "name":"Rockridge (Oakland)"],
+    ["code":"sbrn", "name":"San Bruno"],
+    ["code":"sfia", "name":"San Francisco Int'l Airport"],
+    ["code":"sanl", "name":"San Leandro"],
+    ["code":"shay", "name":"South Hayward"],
+    ["code":"ssan", "name":"South San Francisco"],
+    ["code":"ucty", "name":"Union City"],
+    ["code":"warm", "name":"Warm Springs/South Fremont"],
+    ["code":"wcrk", "name":"Walnut Creek"],
+    ["code":"wdub", "name":"West Dublin"],
+    ["code":"woak", "name":"West Oakland"]
+]
