9d9699267f0448177675c1341bda5319584806ba
Danny Glover <danny@plastiq.com>
2017-05-01 06:38:47 -0700

    adding Podfile, AFNetworking, and API Client file

---
+++ BartApp/BartClient.swift
@@ -0,0 +1,29 @@
+//
+//  TwitterClient.swift
+//  DGTwitter
+//
+//  Created by Danny Glover on 4/13/17.
+//  Copyright © 2017 Danny Glover. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class BartClient {
+    
+    static let sharedInstance = BartClient()
+    
+    let baseURL = URL(string:"http://api.bart.gov/api/sched.aspx?key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1")
+    
+    
+
+    
+//    static let baseURL = URL(string:"http://api.bart.gov/api/sched.aspx?cmd=depart&orig=ASHB&dest=CIVC&date=now&key=MW9S-E7SL-26DU-VV8V&b=2&a=2&l=1")
+
+    
+//    static let sharedInstance = BartClient(baseURL: URL(string:"https://api.twitter.com"), consumerKey:"Mnj3jrPySwJjj7LWvHkQFYjWM", consumerSecret: "tHWjis54o9cZt3XN352YHsSYk7LTob2wlr4rS9Jug1QjmaKHyP")
+    
+
+    
+    
+}
